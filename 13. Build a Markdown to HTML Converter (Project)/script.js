dd709fbcf63396b62727ae45eb8dac74ecfe6d4e
miguel1715 <miguelcruz.1715@gmail.com>
2026-08-26 19:39:25 +0100

    add markdown to HTML converter - first certification project using regex and DOM manipulation

---
+++ 13. Build a Markdown to HTML Converter (Project)/script.js
@@ -0,0 +1,26 @@
+function convertMarkdown() {
+    const markdownText = document.getElementById("markdown-input").value;
+
+    const html = markdownText
+        .replace(/^# (.+)/gm, "<h1>$1</h1>")
+        .replace(/^## (.+)/gm, "<h2>$1</h2>")
+        .replace(/^### (.+)/gm, "<h3>$1</h3>")
+        .replace(/\*\*(.+?)\*\*|__(.+?)__/g, "<strong>$1$2</strong>")
+        .replace(/\*(.+?)\*|_(.+?)_/g, "<em>$1$2</em>")
+        .replace(/!\[(.+?)\]\((.+?)\)/g, '<img alt="$1" src="$2">')
+        .replace(/\[(.+?)\]\((.+?)\)/g, '<a href="$2">$1</a>')
+        .replace(/^> (.+)/gm, '<blockquote>$1</blockquote>')
+
+    return html;
+}
+
+const inputText = document.getElementById("markdown-input");
+const htmlOutput = document.getElementById("html-output");
+const htmlPreview = document.getElementById("preview");
+
+inputText.addEventListener("input", () => {
+    const conversion = convertMarkdown();
+
+    htmlOutput.innerText = conversion;
+    htmlPreview.innerHTML = conversion;
+})
